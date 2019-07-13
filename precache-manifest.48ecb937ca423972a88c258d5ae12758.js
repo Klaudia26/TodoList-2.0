eb1edb174d5bfca05e3e5c60ddac7b5006ab901e
Klaudia <klaudia.krzywda@gmail.com>
2019-07-13 12:15:08 +0100

    Updates

---
+++ precache-manifest.48ecb937ca423972a88c258d5ae12758.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fb4e416a74784f67805c33c3710d05",
+    "url": "/TodoList-2.0/index.html"
+  },
+  {
+    "revision": "f9442d9400ffd897ed49",
+    "url": "/TodoList-2.0/static/js/2.7dfacdc2.chunk.js"
+  },
+  {
+    "revision": "87e6e0d283779447bc3c",
+    "url": "/TodoList-2.0/static/js/main.6718d4a4.chunk.js"
+  },
+  {
+    "revision": "8bef226f540dfd42cb45",
+    "url": "/TodoList-2.0/static/js/runtime~main.5dce87f3.js"
+  }
+]);
